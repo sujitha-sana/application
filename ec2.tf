d80477d839dc99d99247b639c5acb5bee72e90aa
suji <sujithasana1997@gmail.com>
2022-09-17 13:30:13 +0530

    Testing

--- ec2.tf
+++ ec2.tf
@@ -18,7 +18,7 @@
 #   owners      		 = ["679593333241"]
 #   most_recent 		 = true
 
-#   filter {
+##   filter {
 #     name   		 = "name"
 #     values 		 = ["CentOS Linux 7 x86_64 HVM EBS *"]
 #   }
